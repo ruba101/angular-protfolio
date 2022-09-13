8c5449d13b47e46e453b17922b06a54351e0c691
Ruba <dilrubaahmed101@gmail.com>
2022-09-14 01:50:00 +0200

    experience added

--- src/app/modules/dashboard/components/experience-item/experience-item.component.ts
+++ src/app/modules/dashboard/components/experience-item/experience-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { IExperienceItem } from '../experience/experience.component';
 
 @Component({
   selector: 'app-experience-item',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experience-item.component.scss']
 })
 export class ExperienceItemComponent implements OnInit {
+
+  @Input() item:IExperienceItem = {date:'', title:'', institution:'', description:''};
 
   constructor() { }
 
--- src/app/modules/dashboard/components/experience/experience.component.ts
+++ src/app/modules/dashboard/components/experience/experience.component.ts
@@ -1,11 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface IExperienceItem{
+  date: string,
+  title:string,
+  institution:string,
+  description:string
+}
+
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.scss']
 })
 export class ExperienceComponent implements OnInit {
+
+  experienceItems:IExperienceItem[] = [
+    {
+      date:'01.09.2022 - Present',
+      title:'Frontend Developer',
+      institution:'Upwork',
+      description: 'Major in Finance'
+    },
+    {
+      date:'01.02.2019 - 31.08.2020',
+      title:'Frontend Developer',
+      institution:'Urbok Studio',
+      description: ''
+    },
+    {
+      date:'01.12.2018 - 31.01.2019',
+      title:'Trainee',
+      institution:'Urbok Studio',
+      description: ''
+    }
+  ];
 
   constructor() { }
 

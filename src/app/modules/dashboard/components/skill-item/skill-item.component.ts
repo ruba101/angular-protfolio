40e16540ee53082b09bb78ea652fa369ae3d99ee
Ruba <dilrubaahmed101@gmail.com>
2022-09-13 23:47:12 +0200

    skill item animation added

--- src/app/modules/dashboard/components/skill-item/skill-item.component.ts
+++ src/app/modules/dashboard/components/skill-item/skill-item.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ISkillItem } from '../skills/skills.component';
+import { ISkillItem, SkillsComponent } from '../skills/skills.component';
 
 @Component({
   selector: 'app-skill-item',
@@ -10,9 +10,24 @@ export class SkillItemComponent implements OnInit {
 
   @Input() item: ISkillItem = {title:'', value: 0, icon: ''};
 
+  currentValue:number = 100;
+  increment:number = 5;
+  delay:number = 50;
+  self:any = this;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.currentValue = 0;
+    this.increaseValue();
+  }
+
+  increaseValue(){
+    this.currentValue += this.increment;
+    this.currentValue = Math.min(this.item.value, this.currentValue);
+    if(this.currentValue < this.item.value){
+      setTimeout(() => {this.increaseValue();}, this.delay);
+    }
   }
 
 }
